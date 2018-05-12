745bc4209d0ab566e4f1d7ddfb0308538a1f7b8b
ludmilla <l.carlier@live.nl>
2018-05-12 17:29:16 +0200

    number of guesses is shown

--- src/components/wrongGuess.js
+++ src/components/wrongGuess.js
@@ -1,17 +1,21 @@
 import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { showGuess } from '../lib/gameLogic'
+import { showGuess, wrongGuessCount } from '../lib/gameLogic'
 
 class Word extends PureComponent {
   static propTypes = {
     hiddenWord: PropTypes.string.isRequired,
+    lettersGuessed: PropTypes.string.isRequired,
+    numberOfTries: PropTypes.number.isRequired
   }
 
   render() {
     return (
       <div className="gameStuff">
         <h2>{this.props.hiddenWord}</h2>
+        <p>This many tries already newb: {this.props.numberOfTries}</p>
+        <p>These letters you already used : {this.props.lettersGuessed}</p>
       </div>
     )
   }
@@ -20,6 +24,8 @@ class Word extends PureComponent {
 const mapStateToProps = ({ selectedWord, lettersGuessed }) => {
   return {
     hiddenWord: showGuess(selectedWord, lettersGuessed),
+    numberOfTries: wrongGuessCount(selectedWord, lettersGuessed),
+    lettersGuessed: lettersGuessed.join(', ')
   }
 }
 
