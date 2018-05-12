2dd2ee9dfb37b191b102e2913536012e70c26269
ludmilla <l.carlier@live.nl>
2018-05-12 10:08:16 +0200

    cleaned up default app

---
+++ src/actions/weetNogNiet.js
@@ -0,0 +1,13 @@
+// export const FETCHED_DETAILED_PRODUCT = 'FETCHED_DETAILED_PRODUCT'
+//
+//
+//
+// export const fetchProduct = (productId) => (dispatch) => {
+//     request
+//     .get(`${baseUrl}/products/${productId}`)
+//     .then(response => dispatch({
+//         type: FETCHED_DETAILED_PRODUCT,
+//         payload: response.body
+//     }))
+//     .catch(err => alert(err))
+// }
---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+// import products from './products'
+// import product from './product'
+// import users from './users'
+// import login from './login'
+//
+//
+// export default {
+// 	products,
+// 	product,
+// 	users,
+// 	login
+// }
